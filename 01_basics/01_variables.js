c40887090a0358a8a3a5b3d9c8eddb6ec9e5b6b5
sc3ways <147125919+sc3ways@users.noreply.github.com>
2023-10-08 00:01:09 +0000

    Javascript Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,34 @@
+const accountId = "123425";
+let accountEmail = "sc3ways@3ways.in";
+var accountPassword = "#sc3ways@123";
+accountCity = "New Delhi";
+var accountState;
+
+//print single variable one by one.
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+// console.log(accountState);
+
+//Change variables
+//accountId = "21321";
+accountEmail = "web@sc.in";
+accountPassword = "#321dgir23";
+accountCity = "Ghaziabad";
+
+//Print variable with new value;
+//console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+/* Note:-
+*** Prefer not to use var keyword because it allows to change value in any scopes or functions.
+*** Const keyword not allow to change variable value.
+*** Let keyword defines local variables
+*/
+
+
+
